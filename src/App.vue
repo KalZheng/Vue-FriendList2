5d3087f592a2b44d173eef1e5966456ac9af463f
Kal Zheng <kal.zheng@inspire-dt.com>
2023-04-25 15:34:17 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>My Friends</h2>
+    <ul v-for="friend in friends" :key="friend.id">
+      <li></li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      friends: [
+        {
+          id: "manuel",
+          name: "Manuel Test",
+          phone: "1234565",
+          email: "manuelTest@remail.com",
+        },
+        {
+          id: "julie",
+          name: "Julie Test",
+          phone: "123124325325",
+          email: "julieTest@remail.com",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,20 @@
+<template>
+  <li>
+    <h2>{{ friend.name }}</h2>
+    <button>Show Details</button>
+    <ul><li>
+        <strong>Phone: </strong> {{ friend.phone }}
+        <strong>Email: </strong> {{ friend.email }}
+    </li></ul>
+  </li>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            detailsAreVisible
+        };
+    },
+};
+</script>
