77624a89568005446df1a0f749c324412f62b13a
Kal Zheng <kal.zheng@inspire-dt.com>
2023-04-26 13:50:36 +0800

    add add friend vue

---
+++ src/components/NewFriend.vue
@@ -0,0 +1,38 @@
+<template>
+  <form>
+    <div>
+      <label for="name">Name: </label
+      ><input v-model="friend.name" id="name" type="text" />
+    </div>
+    <div>
+      <label for="phone">Phone: </label
+      ><input v-model="friend.phone" id="phone" type="text" />
+    </div>
+    <div>
+      <label for="email">Email: </label
+      ><input v-model="friend.email" id="email" type="text" />
+    </div>
+    <button @click.prevent="addFriend">Add Friend</button>
+  </form>
+</template>
+
+<script>
+export default {
+  emits: ["add-friend"],
+  data() {
+    return {
+      friend: {
+        name: "",
+        phone: "",
+        email: "",
+      },
+    };
+  },
+  methods: {
+    addFriend() {
+      const friendData = this.friend;
+      this.$emit("add-friend", friendData);
+    },
+  },
+};
+</script>
